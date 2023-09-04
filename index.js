cea972c98accc4d7350c177c14395265220907fd
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-09-04 20:14:07 +0900

    refactor middleware

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
 //router
 const router = require("./routes");
