6fc6374c253a666723830a1a5e4d7e89cd3f77ef
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-09-04 20:10:02 +0900

    refactor middleware

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/public"));
 
-// 정적 파일
-app.use('image', express.static(__dirname + '/public/image'));
-// 추후 src 경로에서 "/image/파일명" 으로 접근 가능
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/public', express.static(__dirname + '/public'))
