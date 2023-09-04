2819c6f45781f8e45b4bf0c4521024b7c6c535a3
dbals627 <gkjm123@naver.com>
2023-09-04 19:30:23 +0900

    feat board

--- index.js
+++ index.js
@@ -6,10 +6,19 @@ app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/public', express.static(__dirname + '/public'))
 
 //router
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.get('/board', (req, res) => {
+    res.render('board');
+});
+
+app.get('/boardwrite', (req, res) => {
+    res.render('boardwrite');
 });
 
 app.listen(PORT, () => {
