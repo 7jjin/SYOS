289065c42edf96cc3c86c394ce2ca8f2347bbad1
jaejae990921 <jaejae990921@naver.com>
2023-09-05 17:03:50 +0900

    refactor prettier
    prettier 적용

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 //router
 const router = require("./routes");
