2e3f5fe2d78851f180a6b8b6add34fa1cab3f94b
7jjin <wlsgud6305@naver.com>
2023-10-07 13:47:03 +0900

    text
    test

--- config/config.js
+++ config/config.js
@@ -1,21 +1,20 @@
-require('dotenv').config();
+// require('dotenv').config();
 const env = process.env;
- 
+
 const development = {
   username: 'mysql_prac',
   password: '12345678',
   database: 'kdt9',
   host: '127.0.0.1',
-  dialect: 'mysql'
-}
+  dialect: 'mysql',
+};
 
 const production = {
   username: env.RDS_USERNAME,
   password: env.RDS_PASSWORD,
   database: env.RDS_DATABASE,
   host: env.RDS_HOST,
-  dialect: "mysql",
+  dialect: 'mysql',
 };
- 
 
 module.exports = { production, development };
